d4866303f9d2b3cc038d1dad91a0624c2afb449f
Zombieboyroy <17916475-Zombieboyroy@users.noreply.replit.com>
2023-06-29 14:30:53 +0000

    Just cleaned up formatting, tabbing lines for readability and adding comments to label stuff

--- .ccls-cache/@home@runner@Character-Sheet/diceroller.cpp
+++ .ccls-cache/@home@runner@Character-Sheet/diceroller.cpp
@@ -4,66 +4,67 @@
 int currentSeed;
 std::string seedinput;
 
+//RandomSetUp class functions
 void RandomSetUp::randomSeed() 
-  {
-    currentSeed = time(NULL);
-    srand(time(NULL));
-  }
+{
+  currentSeed = time(NULL);
+  srand(time(NULL));
+}
 
 void RandomSetUp::userSeed()
-  {
-    // Prompt for user to input their seed
-    std::cout << "\nEnter seed\n";
-    std::cin >> seedinput;
+{
+  // Prompt for user to input their seed
+  std::cout << "\nEnter seed\n";
+  std::cin >> seedinput;
 
-    // Use std::hash to convert the input string into an integer
-    std::hash<std::string> strtohash;
-    int seed = static_cast<int>(strtohash(seedinput));
-    currentSeed = seed;
+  // Use std::hash to convert the input string into an integer
+  std::hash<std::string> strtohash;
+  int seed = static_cast<int>(strtohash(seedinput));
+  currentSeed = seed;
 
-    // Providing the custom seed value
-    srand(seed);
-  }
+  // Providing the custom seed value
+  srand(seed);
+}
 
-
+//Dice class functions
 int Dice::Dfour()
-  {
-    int x = rand() % 4 + 1;
-    return x;
-  }
+{
+  int x = rand() % 4 + 1;
+  return x;
+}
 
 int Dice::Dsix()
-  {
-    int x = rand() % 6 + 1;
-    return x;
-  }
+{
+  int x = rand() % 6 + 1;
+  return x;
+}
 
 int Dice::Deight()
-  {
-    int x = rand() % 8 + 1;
-    return x;
-  }
+{
+  int x = rand() % 8 + 1;
+  return x;
+}
 
 int Dice::Dten()
-  {
-    int x = rand() % 10 + 1;
-    return x;
-  }
+{
+  int x = rand() % 10 + 1;
+  return x;
+}
 
 int Dice::Dtwelve()
-  {
-    int x = rand() % 12 + 1;
-    return x;
-  }
+{
+  int x = rand() % 12 + 1;
+  return x;
+}
 
 int Dice::Dtwenty()
-  {
-    int x = rand() % 20 + 1;
-    return x;
-  }
+{
+  int x = rand() % 20 + 1;
+  return x;
+}
 
 int Dice::Donehundred()
-  {
-    int x = rand() % 100 + 1;
-    return x;
-  }
+{
+  int x = rand() % 100 + 1;
+  return x;
+}
